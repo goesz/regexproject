37018ee3bf10788bac8799d63bb26249118f6109
Rafael Goes <goesdeveloper@gmail.com>
2023-10-09 13:53:22 -0300

    Update script.js
    Minimum viable product of my project.

--- script.js
+++ script.js
@@ -4,11 +4,66 @@ function showPass() {
   if (x.type === "password") {
     y.type = "text";
     x.type = "text";
-    document.getElementById("showPassword").value = 'Esconder senha';
   } else {
     x.type = "password";
     y.type = "password";
-    document.getElementById("showPassword").value = 'Mostrar senha';
+  }
+}
+
+function verifyPassw(senha) {
+  if (senha.length < 6) {
+    return false;
   }
 
+  if (!/[!@#\$%\^&\*]/.test(senha)) {
+    return false;
+  }
+
+  if (!/[A-Z]/.test(senha)) {
+    return false;
+  }
+
+  if (!/[a-z]/.test(senha)) {
+    return false;
+  }
+
+  if (!/[0-9]/.test(senha)) {
+    return false;
+  }
+
+  if (/(\w)\1\1/.test(senha)) {
+    return false;
+  }
+
+  if (/\d{3}/.test(senha)) {
+    return false;
+  }
+
+  if (/987|876|765|654|543|432|321/.test(senha)) {
+    return false;
+  }
+
+  return true;
+}
+
+function fazerLogin() {
+  const passw1 = document.getElementById("password").value;
+  const passw2 = document.getElementById("password2").value;
+
+  if (passw1 === "Rafael_33855072" && passw2 === "Rafael_27055833") {
+    document.getElementById("escrever").innerHTML = "Nome dos criadores OK!";
+  } else if (passw1 === passw2 && verifyPassw(passw1)) {
+    document.getElementById("escrever").innerHTML = "Login bem-sucedido";
+  } else if (passw1 !== passw2) {
+    document.getElementById("escrever").innerHTML = "As senhas não coincidem";
+  }
+  else {
+    document.getElementById("escrever").innerHTML = "Sua senha não atende aos requisitos de validação.";
+    
+    requisitosSenha.style.display = "block";
+    document.querySelector(".box2").style.bottom = "-50px";
+    document.querySelector(".box2").style.height = "500px";
+    document.querySelector(".box2").style.width = "600px";
+    document.querySelector("h1").style.display = "none";
+  }
 }
